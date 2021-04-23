d7a3613f64725eddfce95004db792b4419dc9027
Yaro <yshmarov@gmail.com>
2021-04-23 10:03:55 +0000

    rename app

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HowtoActsAsVotable
+module Superails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
