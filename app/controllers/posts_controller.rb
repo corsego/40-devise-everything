90a4f5b1da5a3bdd4da8f3eae5a290f4fcc6a223
Yaro <yshmarov@gmail.com>
2021-05-02 09:13:09 +0000

    styling posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def show
