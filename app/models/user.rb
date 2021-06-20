f5e519ebb7c22150024a6027186f05eb651f81f8
Yaro <yshmarov@gmail.com>
2021-06-20 14:52:00 +0000

    devise confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable,
+         :confirmable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :users, only: [:index]
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'users/confirmations' }
   root "dashboard#index"
   get 'dashboard/index'
 end
