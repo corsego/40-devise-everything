e72151d4c57043a0607b5bb558748ad9621539b4
Yaro <yshmarov@gmail.com>
2021-06-20 15:17:42 +0000

    devise lockable if password wrong 3 times

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
+  # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable,
-         :confirmable
+         :confirmable, :lockable
 end
