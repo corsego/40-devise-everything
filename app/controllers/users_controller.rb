ce1bd0ee4567a0a2f08238da1402bfda4dfd96bc
Yaro <yshmarov@gmail.com>
2021-06-20 15:25:00 +0000

    ban or unban users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def ban
+    @user = User.find(params[:id])
+    if @user.access_locked?
+      @user.unlock_access!
+    else
+      @user.lock_access!
+    end
+    redirect_to users_path, notice: "User access locked: #{@user.access_locked?}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    member do
+      patch :ban
+    end
+  end
   devise_for :users, controllers: { confirmations: 'users/confirmations' }
   root "dashboard#index"
   get 'dashboard/index'
